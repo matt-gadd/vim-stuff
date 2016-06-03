f737ee316c8280700027a92f394056ef85eb5b0a
Matthew Gadd <mgadd@sitepen.com>
2016-06-03 22:35:56 +0100

    copy vimrc

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 brew install nodejs
 brew install neovim/neovim/neovim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
 brew install python3
 pip3 install --user neovim
 npm install -g typescript
