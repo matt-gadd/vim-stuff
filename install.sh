187de709217ac90d02ef4ff1345eb0c037542af7
Matthew Gadd <mgadd@sitepen.com>
2016-06-06 17:14:10 +0100

    Make sure to use nvim, and point to vim conf and dirs

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 brew install nodejs
 brew install neovim/neovim/neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 brew install python3
 pip3 install --user neovim
 npm install -g typescript
-vim +PluginInstall +qall
+nvim +PluginInstall +qall
 make -C ~/.vim/bundle/vimproc.vim
 ~/.vim/bundle/YouCompleteMe/install.sh
