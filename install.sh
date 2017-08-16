9e2b27d050caa3c71345b848af2f19bcd00792d9
Matthew Gadd <mgadd@sitepen.com>
2017-08-16 18:13:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 brew install nodejs
 brew install neovim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+cp .vimrc ~/.vimrc
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc ~/.vimrc
 brew install python3
 pip3 install --user neovim
 npm install -g typescript
